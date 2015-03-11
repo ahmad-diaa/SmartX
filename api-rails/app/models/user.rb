b2314ddf0650a27de235dce219c9c7408479ae5b
AmirMaurice <amir.riad@student.guc.edu.eg>
2015-03-11 17:30:14 +0200

    api in rails created user model & controller,added routes and column digest to user through migration

---
+++ api-rails/app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+	has_secure_password    
+	validates :name,  presence: true, length: { maximum: 25 }, uniqueness: { case_sensitive: false }
+	validates :password,  presence: true, length: { minimum: 6}
+	def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+end
---
+++ api-rails/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  namespace :api, :defaults => {:format => :json} do
+    namespace :v1 do
+      get     "/users",     to: "users#index"
+      post    "/users",     to: "users#create"
+      get     "/users/:id", to: "users#show"
+      put     "/users/:id", to: "users#update"
+      delete  "/users/:id", to: "users#destroy"
+      resources :users
+    end
+  end
+end
