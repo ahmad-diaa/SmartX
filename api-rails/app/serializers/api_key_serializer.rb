fd81cef768b2f56380aa34ada4bb345be63207e6
ahmad-diaa <ahmaddiaa93@gmail.com>
2015-03-12 13:01:04 +0200

    testing error

---
+++ api-rails/app/serializers/api_key_serializer.rb
@@ -0,0 +1,4 @@
+class APIKeySerializer < ActiveModel::Serializer
+  attributes :id, :access_token
+  has_one :user, embed: :id
+end
--- api-rails/config/routes.rb
+++ api-rails/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :api_keys, except: [:new, :edit]
   namespace :api, :defaults => {:format => :json} do
     namespace :v1 do
       get     "/users",     to: "users#index"
