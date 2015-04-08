5701c704b4d615d621ff46ddf3829d8b40e82472
maggie-moheb <maggiemoheb@gmail.com>
2015-04-08 21:29:26 +0200

    Added rails test for rename method #17

--- api-rails/test/controllers/rooms_controller_test.rb
+++ api-rails/test/controllers/rooms_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class RoomsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ #tests that the controller creates successfully a note when is called with right parameters
+	test "#rename room" do
+  	  put 'update', {user_id: '1', id: '1', 
+      		room: {
+        		name: 'Billy Blowers'
+      		}
+    	}
+    	get 'show', {user_id: '1', id: '1'}
+    	results = JSON.parse(response.body)
+    	assert results['name'] == 'Billy Blowers'
+  	end
 end
