e512ef59b03f5a97d5413e951556dfbbf7eb531a
maggie-moheb <maggiemoheb@gmail.com>
2015-04-08 22:02:59 +0200

    Fixed punctuation in rooms controller test and added the UML diagram #17

--- api-rails/test/controllers/rooms_controller_test.rb
+++ api-rails/test/controllers/rooms_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class RoomsControllerTest < ActionController::TestCase
-#tests the rooms_controller
+#Tests the rooms_controller.
 
-	 #tests that the controller updates the name successfully to a room when is called with right parameters
+	 #Tests that the controller updates the name successfully to a room when is called with right parameters.
 	test "#rename room" do
   	  put 'update', {user_id: '1', id: '1', 
       		room: {
@@ -14,7 +14,7 @@ class RoomsControllerTest < ActionController::TestCase
     	results = JSON.parse(response.body)
     	assert results['name'] == 'Billy Blowers'
   	end
-  	#tests that the controller will not change the name when passed wrong parameters (empty room name)
+  	#Tests that the controller will not change the name when passed wrong parameters (empty room name).
   	test "#rename room with wrong parameters" do
   	  put 'update', {user_id: '1', id: '1', 
       		room: {
