22a30a19fd5770aa958a89c728432770d8e8fbc5
AmirMaurice <amir.riad@student.guc.edu.eg>
2015-04-08 17:40:01 +0200

    documentation and sequence diagram added #27

--- SmartX/app/src/main/java/com/smartx/cookies/smartx/addRoomsActivity.java
+++ SmartX/app/src/main/java/com/smartx/cookies/smartx/addRoomsActivity.java
@@ -15,41 +15,94 @@ import retrofit.RestAdapter;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
+/**
+ *SE Sprint1
+ *addRoomsActivity.java
+ * Purpose: Add rooms.
+ *
+ * @author Amir
+ */
+
 public class addRoomsActivity extends Activity{
-    Button addRoomButton;
-    EditText roomName;
-    public static int count = -1;
-    String ENDPOINT = "http://192.168.1.2:3000/";
-    int userID;
-    int[] photos = new int[]{ R.drawable.one ,
+
+    /**
+     * Button clicked to add a room.
+     */
+    private Button addRoomButton;
+
+    /**
+     * Used in randomizing photos for rooms.
+     */
+    private static int count = -1;
+
+    /**
+     * Endpoint composed of the ip address of network
+     plus port number of server.
+     */
+    private String endpoint = "http://192.168.1.2:3000/";
+
+    /**
+     * Holds user's id saved in shared preferences.
+     */
+    private int userID;
+
+
+    /**
+     * Holds 9 different photos for rooms.
+     */
+     private int[] photos = new int[]{ R.drawable.one ,
             R.drawable.two ,R.drawable.three ,R.drawable.four ,R.drawable.five ,
             R.drawable.six ,R.drawable.seven ,R.drawable.eight ,R.drawable.nine};
 
+    /**
+     *Called when the activity is starting.
+     Get userID from shared preferences and Get button from View.
+     *
+     * @param savedInstanceState if the activity is being
+    re-initialized after previously being shut down then
+    this Bundle  contains the data it most recently supplied.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addrooms);
-        final SharedPreferences  mSharedPreference= PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-        userID=(mSharedPreference.getInt("userID", 1));
+        final SharedPreferences  SHARED_PREFERENCE=
+                PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        userID=(SHARED_PREFERENCE.getInt("userID", 1));
         addRoomButton = (Button) findViewById(R.id.addRoomButton);
     }
 
+    /**
+     * This method is used to randomize photos for rooms.
+     *
+     * @return integer value from zero to eight to select a photo for the room.
+     */
     public int randomIcon(){
-         count = (count + 1)%9 ;
-         return count;
+        count = (count + 1)%9 ;
+        return count;
     }
 
+
+    /**
+     * This method is called when the button is clicked.
+     addRoom method posts to rails a new room to be created for current user then
+     Activity ViewRooms starts showing all user's rooms including the new one.
+     *
+     * @param v the view that was clicked.
+     */
     public void addRoomButton(View v) {
-        roomName = (EditText) findViewById(R.id.roomName);
+        EditText roomName = (EditText) findViewById(R.id.roomName);
         Room room = new Room(roomName.getText().toString());
-        RestAdapter adapter = new RestAdapter.Builder().setEndpoint(ENDPOINT).build();
+        RestAdapter adapter =
+                new RestAdapter.Builder().setEndpoint(endpoint).build();
         myAPI api = adapter.create(myAPI.class);
         room.setPhoto(photos[randomIcon()] + "");
         api.addRoom((userID + ""), room.getName(), room.getPhoto(), room.getId()+"", new Callback<Room>() {
 
             @Override
             public void success(Room room, Response response) {
-                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(addRoomsActivity.this);
+                SharedPreferences prefs =
+                        PreferenceManager.getDefaultSharedPreferences(addRoomsActivity.this);
                 SharedPreferences.Editor editor = prefs.edit();
                 editor.putInt("roomID",room.getId());
                 editor.commit();
@@ -58,9 +111,88 @@ public class addRoomsActivity extends Activity{
 
             @Override
             public void failure(RetrofitError error) {
-            Toast.makeText(getApplicationContext(), "Cannot add room!", Toast.LENGTH_LONG).show();
-            throw error;
+                Toast.makeText(getApplicationContext(), "Cannot add room!", Toast.LENGTH_LONG).show();
+                throw error;
             }
         });
     }
+
+
+
+    /**
+     *get the button clicked to add a room.
+     *
+     * @return button
+     */
+    public Button getButton() {
+        return addRoomButton;
+    }
+
+    /**
+     *set the button clicked to add a room.
+     *
+     * @param addRoomButton
+     */
+    public void setButton(Button addRoomButton) {
+        this.addRoomButton = addRoomButton;
+    }
+
+    /**
+     *get photos for rooms.
+     *
+     * @return photos
+     */
+    public int[] getPhotos() {
+        return photos;
+    }
+
+    /**
+     *set photos for rooms.
+     *
+     * @param photos
+     */
+    public void setUserID(int [] photos) {
+        this.photos = photos;
+    }
+
+    /**
+     *get the endpoint composed of the ip address of network
+     plus the port number of server.
+     *
+     * @return the endpoint
+     */
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    /**
+     *set the endpoint composed of the ip address of network
+     plus the port number of server.
+     *
+     * @param endpoint
+     */
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    /**
+     *get id of the user.
+     *
+     * @return primary key of the user.
+     */
+    public int getUserID() {
+        return userID;
+    }
+
+    /**
+     *set id of the user.
+     *
+     * @param userID the primary key of the user.
+     */
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+
+
 }
