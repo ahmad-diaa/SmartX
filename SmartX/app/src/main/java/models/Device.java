a811d06f99f8780befc2ef6002fd827fa4ad6697
AmirMaurice <amir.riad@student.guc.edu.eg>
2015-04-08 19:14:59 +0200

    documentation and sequence diagram added #20

--- SmartX/app/src/main/java/models/Device.java
+++ SmartX/app/src/main/java/models/Device.java
@@ -1,14 +1,42 @@
 package models;
 
-public class Device {
-    int userID;
-    String name;
-    String status;
-    int roomID;
-    String deviceID;
-    public Device(){
+/**
+ *SE Sprint1
+ *Device.java
+ *
+ * @author Amir
+ */
 
-    }
+public class Device {
+    /**
+     * The id of the user having the device.
+     */
+    int userID;
+
+    /**
+     * The type of the device.
+     */
+    String name;
+
+    /**
+     * The status of the device: on or off.
+     */
+    String status;
+
+    /**
+     * The id of the room to which the device belongs.
+     */
+    int roomID;
+
+    /**
+     * The primary key of device.
+     */
+    String deviceID;
+
+
+    /**
+     * Constructor to create a device with given parameters.
+     */
 
     public Device( String name, int roomID, int userID, String deviceID, String status) {
         this.name = name;
@@ -18,45 +46,96 @@ public class Device {
         this.status=status;
     }
 
+    /**
+     * get the id of the user having the device.
+     *
+     * @return the id of the user.
+     */
     public int getUserID() {
         return userID;
     }
 
+    /**
+     * set the id of the user having the device.
+     *
+     * @param userID the id of the user.
+     */
     public void setUserID(int userID) {
         this.userID = userID;
     }
 
+    /**
+     * get the primary key of device.
+     *
+     * @return id of the device.
+     */
     public String getDeviceID() {
         return deviceID;
     }
 
-    public void setDeviceID(String type) {
+    /**
+     * set get the primary key of device.
+     *
+     * @param deviceID the id of device.
+     */
+    public void setDeviceID(String deviceID) {
         this.deviceID= deviceID;
     }
 
+    /**
+     * get type of the device.
+     *
+     * @return type of the device.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * set type of the device.
+     *
+     * @param name the type of the device.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * get the status of the device: on or off. 
+     *
+     * @return status of the device.
+     */
     public String getStatus() {
         return status;
     }
 
-    public void setStatus(String name) {
+    /**
+     * set the status of the device: on or off. 
+     *
+     * @param status
+     */
+    public void setStatus(String status) {
         this.status = status;
     }
 
+    /**
+     * get the id of the room to which the device belongs.
+     *
+     * @return id of the room.
+     */
     public int getRoomID() {
         return roomID;
     }
 
+    /**
+     * set the id of the room to which the device belongs.
+     *
+     * @param roomID the id of the room.
+     */
     public void setRoomID(int roomID) {
         this.roomID = roomID;
     }
 
 
 }
+
