b5331756fdc19f92a425052d6690a19ae4231f4c
maggie-moheb <maggiemoheb@gmail.com>
2015-04-08 21:50:47 +0200

    Added test for wrong parameters for rename room in rails and documentation #17

--- api-rails/test/controllers/rooms_controller_test.rb
+++ api-rails/test/controllers/rooms_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class RoomsControllerTest < ActionController::TestCase
- #tests that the controller creates successfully a note when is called with right parameters
+#tests the rooms_controller
+
+	 #tests that the controller updates the name successfully to a room when is called with right parameters
 	test "#rename room" do
   	  put 'update', {user_id: '1', id: '1', 
       		room: {
@@ -11,5 +13,16 @@ class RoomsControllerTest < ActionController::TestCase
     	get 'show', {user_id: '1', id: '1'}
     	results = JSON.parse(response.body)
     	assert results['name'] == 'Billy Blowers'
+  	end
+  	#tests that the controller will not change the name when passed wrong parameters (empty room name)
+  	test "#rename room with wrong parameters" do
+  	  put 'update', {user_id: '1', id: '1', 
+      		room: {
+        		name: ''
+      		}
+    	}
+    	get 'show', {user_id: '1', id: '1'}
+    	results = JSON.parse(response.body)
+    	assert results.size > 0 
   	end
 end
