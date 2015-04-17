5a5b295fe399880497a09fa78758e1a81d96b4b0
Youmna-salah <umnasalah@yahoo.com>
2015-04-17 10:29:59 +0200

    found a lost rails clicker tetst

--- api-rails/test/models/clicker_test.rb
+++ api-rails/test/models/clicker_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
-
 class ClickerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ 	
+ 	test "#create Clicker" do
+  	  post 'create', {user_id: '1', room_id: '1', device_device_id: '1',
+      		clickers: {
+        		command:'TV',
+        		user_id: '1',
+        		room_id: '1',
+        		device_id:'1'
+      		}
+    	}
+    	results = JSON.parse(response.body)
+    	assert results['id'] > 0
+    #	assert true
+  	end
+
 end
