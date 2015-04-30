d79c78c8f85e6ac53beb24d087f5b6740d3b94f8
AmirMaurice <amir.riad@student.guc.edu.eg>
2015-04-30 20:14:38 +0200

    sideBar added to tvClickerActivity #48

--- SmartX/app/src/main/java/com/smartx/cookies/smartx/TvClickerActivity.java
+++ SmartX/app/src/main/java/com/smartx/cookies/smartx/TvClickerActivity.java
@@ -5,8 +5,14 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Switch;
 import android.widget.Toast;
@@ -31,6 +37,15 @@ public class TvClickerActivity extends Activity {
     boolean on;//initial current state of device
     SharedPreferences mSharedPreference;//Used to get data from previous sessions
     Clicker TvClicker;
+    String titles[] = {"View Favorites","View Rooms","Edit Information","Change Password","Contact us","About us","Logout"};
+    int icons[] = {R.mipmap.star,R.mipmap.room,R.mipmap.pencil,R.mipmap.lock,R.mipmap.help,R.mipmap.home,R.mipmap.bye};
+    String name ;
+    int profile = R.mipmap.smartorange2;
+    RecyclerView mRecyclerView;
+    RecyclerView.Adapter mAdapter;
+    RecyclerView.LayoutManager mLayoutManager;
+    DrawerLayout Drawer;
+    ActionBarDrawerToggle mDrawerToggle;
 
     /**
      * clickerId getter
@@ -113,6 +128,61 @@ public class TvClickerActivity extends Activity {
             }
         });
         checkPreviousState();
+        mRecyclerView = (RecyclerView) findViewById(R.id.RecyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        final SharedPreferences mSharedPreference = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        name = (mSharedPreference.getString("Name", ""));
+        mAdapter = new SideBarAdapter(titles,icons,name,profile,this);
+        mRecyclerView.setAdapter(mAdapter);
+        final GestureDetector mGestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+
+            @Override public boolean onSingleTapUp(MotionEvent e) {
+                return true;
+            }
+
+        });
+        mRecyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView recyclerView, MotionEvent motionEvent) {
+                View child = recyclerView.findChildViewUnder(motionEvent.getX(),motionEvent.getY());
+                if(child!=null && mGestureDetector.onTouchEvent(motionEvent)){
+                    Drawer.closeDrawers();
+                    switch (recyclerView.getChildPosition(child)){
+                        case 1: startActivity(new Intent(getApplicationContext(), addRoomsActivity.class));break;
+                        case 2: startActivity(new Intent(getApplicationContext(), ViewRooms.class));break;
+                        case 3: startActivity(new Intent(getApplicationContext(), changeInfo.class));break;
+                        case 4: startActivity(new Intent(getApplicationContext(), changePassword.class));break;
+                        case 5: startActivity(new Intent(getApplicationContext(), addRoomsActivity.class));break;
+                        case 6: startActivity(new Intent(getApplicationContext(), About_us.class));break;
+                        case 7: startActivity(new Intent(getApplicationContext(), addRoomsActivity.class));break;
+                    }
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView recyclerView, MotionEvent motionEvent) {
+            }
+        });
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        Drawer = (DrawerLayout) findViewById(R.id.DrawerLayout);
+        mDrawerToggle = new ActionBarDrawerToggle(this,Drawer,R.string.openDrawer,R.string.closeDrawer){
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        Drawer.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
     }
 
     @Override
@@ -126,7 +196,7 @@ public class TvClickerActivity extends Activity {
      * called  if the Volume "+" button in tv_clicker layout is clicked.
      * It updates the current clicker command to the recently entered one by calling sendCommand method
      *
-     * @param View
+     * @param v
      */
     public void volumeUP(View v) {
         command = new String("/V/1");
@@ -141,7 +211,7 @@ public class TvClickerActivity extends Activity {
      * called  if the Volume "-" button in tv_clicker layout is clicked.
      * It updates the current clicker command to the recently entered one by calling sendCommand method
      *
-     * @param View
+     * @param v
      */
     public void volumeDown(View v) {
         command = new String("/V/0");
@@ -156,7 +226,7 @@ public class TvClickerActivity extends Activity {
      * called  if the channel "+" button in tv_clicker layout is clicked.
      * It updates the current clicker command to the recently entered one by calling sendCommand method
      *
-     * @param View
+     * @param v
      */
     public void nextChannel(View v) {
         command = new String("/C/1");
@@ -172,7 +242,7 @@ public class TvClickerActivity extends Activity {
      * called  if the channel "+" button in tv_clicker layout is clicked.
      * It updates the current clicker command to the recently entered one by calling sendCommand method
      *
-     * @param View
+     * @param v
      */
     public void previousChannel(View v) {
         command = new String("/C/0");
@@ -190,7 +260,7 @@ public class TvClickerActivity extends Activity {
      * if the device was turned off then the command wont be sent otherwise sendCommand method
      * will send the current command to the Clicker
      *
-     * @param View
+     * @param v
      */
     public void TurnOnOff(View v) {
         on = !on;
